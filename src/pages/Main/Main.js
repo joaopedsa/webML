a2f7e366cbf9cf4c5c8b9711992f313fd6b86e77
joaopedsa <joaopedrosantana1997@hotmail.com.br>
2020-05-30 03:02:24 -0300

    Teste

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useMemo } from 'react' 
+
+import './index.css'
+export default function Main() {
+
+    useEffect(() => {
+    }, [])
+
+    return (
+        <div>
+        </div>
+    )
+}
